5fb9eaa58dbad7b3fb3655eade81900669666c06
Carlton Duffett <cjduffett@comcast.net>
2017-03-28 14:24:53 -0400

    Added Logger

---
+++ logger/logger.go
@@ -0,0 +1,42 @@
+package logger
+
+import "fmt"
+
+// LogLevel indicates the current level of logging, from 0-2:
+// 0 - Info only
+// 1 - Info and Warning
+// 2 - Info, Warning, and Debug
+var LogLevel int
+
+var DefaultLevel = 0
+var InfoLevel = 0
+var WarnLevel = 1
+var DebugLevel = 2
+
+// Info logs non-critical information to the console at any LogLevel.
+func Info(v ...interface{}) {
+	logStork("INFO", v...)
+}
+
+// Error logs critical information to the console at any LogLevel.
+func Error(v ...interface{}) {
+	logStork("ERROR", v...)
+}
+
+// Warning logs warning information to the console at LogLevel 1 or higher.
+func Warning(v ...interface{}) {
+	if LogLevel >= 1 {
+		logStork("WARN", v...)
+	}
+}
+
+// Debug logs debug information to the console at LogLevel 2 or higher.
+func Debug(v ...interface{}) {
+	if LogLevel == 2 {
+		logStork("DEBUG", v...)
+	}
+}
+
+func logStork(level string, v ...interface{}) {
+	fmt.Println(append([]interface{}{fmt.Sprintf("[Stork] [%s] ", level)}, v...)...)
+}
